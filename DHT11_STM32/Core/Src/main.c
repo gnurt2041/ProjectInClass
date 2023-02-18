112d7d45fc269784e02f8fb560bca52fa340fb72
Thanh Trung <71776470+gnurt2041@users.noreply.github.com>
2023-02-18 20:39:08 +0700

    Add files via upload

--- DHT11_STM32/Core/Src/main.c
+++ DHT11_STM32/Core/Src/main.c
@@ -18,8 +18,9 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
-#include "math.h"
 #include "stdio.h"
+#include "math.h"
+#include "string.h"
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 #include "i2c-lcd.h"
@@ -44,6 +45,8 @@ I2C_HandleTypeDef hi2c1;
 
 TIM_HandleTypeDef htim1;
 
+UART_HandleTypeDef huart1;
+
 /* USER CODE BEGIN PV */
 
 /* USER CODE END PV */
@@ -53,6 +56,7 @@ void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_I2C1_Init(void);
 static void MX_TIM1_Init(void);
+static void MX_USART1_UART_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -222,17 +226,19 @@ int main(void)
   MX_GPIO_Init();
   MX_I2C1_Init();
   MX_TIM1_Init();
+  MX_USART1_UART_Init();
   /* USER CODE BEGIN 2 */
-HAL_TIM_Base_Start(&htim1);
+  HAL_TIM_Base_Start(&htim1);
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
   while (1)
-  {
-		 HAL_GPIO_WritePin(GPIOA, GPIO_PIN_7, 0);
+  {  
+		 char result[40];
+		 HAL_GPIO_WritePin(GPIOA, GPIO_PIN_2, 0);
      HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, 0);
-     HAL_GPIO_WritePin(GPIOA, GPIO_PIN_1, 0);
+     HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, 0);
 		 if(DHT11_Start())
     {
       RHI = DHT11_Read(); // Relative humidity integral
@@ -248,38 +254,42 @@ HAL_TIM_Base_Start(&htim1);
         RH = (float)RHI + (float)(RHD/10.0);
         // Can use tCelsius, tFahrenheit and RH for any purposes
       }
-			ftoa(tCelsius, str_tC, 1);
-			ftoa(RH, str_RH, 1);
+			ftoa(tCelsius, str_tC, 2);
+			ftoa(RH, str_RH, 2);
 			lcd_init ();
-			lcd_send_string("Tempeture:");
+			lcd_send_string("Temp:");
 			lcd_send_string(str_tC);
 			lcd_send_string("C");
 	    //HAL_Delay(1000);
 	    lcd_goto_XY(2,0);
-	    lcd_send_string("Humidity:");
+	    lcd_send_string("Humi:");
 			lcd_send_string(str_RH);
 			lcd_send_string("%");
+			strcpy(result,str_tC);
+			strcat(result, " ");
+			strcat(result,str_RH);
+			HAL_UART_Transmit(&huart1, (uint8_t*) result, 9, 1000);
       if (TCI < 15) 
       {
-        HAL_GPIO_WritePin(GPIOA, GPIO_PIN_7, 1);
+        HAL_GPIO_WritePin(GPIOA, GPIO_PIN_2, 1);
         HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, 0);
-        HAL_GPIO_WritePin(GPIOA, GPIO_PIN_1, 0);
+        HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, 0);
       }
       else if (TCI < 25 ) 
-      {
-        HAL_GPIO_WritePin(GPIOA, GPIO_PIN_7, 0);
+      {     
+        HAL_GPIO_WritePin(GPIOA, GPIO_PIN_2, 0);
         HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, 1);
-        HAL_GPIO_WritePin(GPIOA, GPIO_PIN_1, 0);
+        HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, 0);
       }
       else 
       {
-        HAL_GPIO_WritePin(GPIOA, GPIO_PIN_7, 0);
+        HAL_GPIO_WritePin(GPIOA, GPIO_PIN_2, 0);
         HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4, 0);
-        HAL_GPIO_WritePin(GPIOA, GPIO_PIN_1, 1);
+        HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, 1);
       }
 			//lcd_clear_display();
     }
-    HAL_Delay(1000);
+    HAL_Delay(3000);
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
@@ -407,6 +417,39 @@ static void MX_TIM1_Init(void)
 }
 
 /**
+  * @brief USART1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART1_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART1_Init 0 */
+
+  /* USER CODE END USART1_Init 0 */
+
+  /* USER CODE BEGIN USART1_Init 1 */
+
+  /* USER CODE END USART1_Init 1 */
+  huart1.Instance = USART1;
+  huart1.Init.BaudRate = 115200;
+  huart1.Init.WordLength = UART_WORDLENGTH_8B;
+  huart1.Init.StopBits = UART_STOPBITS_1;
+  huart1.Init.Parity = UART_PARITY_NONE;
+  huart1.Init.Mode = UART_MODE_TX_RX;
+  huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart1.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init(&huart1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART1_Init 2 */
+
+  /* USER CODE END USART1_Init 2 */
+
+}
+
+/**
   * @brief GPIO Initialization Function
   * @param None
   * @retval None
@@ -421,24 +464,24 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_1|GPIO_PIN_4|GPIO_PIN_7, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOA, BLUE_Pin|YELLOW_Pin|RED_Pin, GPIO_PIN_RESET);
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_9, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(DHT11_GPIO_Port, DHT11_Pin, GPIO_PIN_RESET);
 
-  /*Configure GPIO pins : PA1 PA4 PA7 */
-  GPIO_InitStruct.Pin = GPIO_PIN_1|GPIO_PIN_4|GPIO_PIN_7;
+  /*Configure GPIO pins : BLUE_Pin YELLOW_Pin RED_Pin */
+  GPIO_InitStruct.Pin = BLUE_Pin|YELLOW_Pin|RED_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
-  /*Configure GPIO pin : PB9 */
-  GPIO_InitStruct.Pin = GPIO_PIN_9;
+  /*Configure GPIO pin : DHT11_Pin */
+  GPIO_InitStruct.Pin = DHT11_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+  HAL_GPIO_Init(DHT11_GPIO_Port, &GPIO_InitStruct);
 
 }
 
